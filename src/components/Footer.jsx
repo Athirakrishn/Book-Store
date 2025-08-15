a4b2205833fda3563c265a67ee20685f0a3cd53d
Athirakrishn <athiraramakrishnan41020@gmail.com>
2025-08-15 13:55:19 +0530

    day 1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+     <div className="d-flex justify-content-center text-align-center" style={{background:"linear-gradient(to right, rgb(0, 255, 200), rgb(2, 104, 117))",width:"100%",height:"40vh"}}>
+<h3 className='text-light' >Contact Us</h3>
+  
+     </div>
+    </>
+  )
+}
+
+export default Footer
