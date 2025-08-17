81026f43a633f6d0e78cfd70c541bf8448b61433
Athirakrishn <athiraramakrishnan41020@gmail.com>
2025-08-17 10:38:59 +0530

    day3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,12 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = ['Home', 'About Us', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = ['Home', 'About Us', 'Books'];
+const settings = ['Profile', 'Contact', 'Logout'];
 
 function Header() {
 
@@ -37,125 +35,153 @@ function Header() {
   };
 
   return (
-    <AppBar position="static" sx={{background:"linear-gradient(to right, rgb(0, 255, 200), rgb(2, 104, 117))"}}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-         <img src=" " alt="" style={{width:"50px",height:"50px", padding:"30px"}} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'white',
-              textDecoration: 'none',
-            }}
-          >
-            <img src="https://i.pinimg.com/736x/ff/a9/32/ffa932be25e0bfa85d6c54b82bff0a27.jpg" alt="" width={'40'} height={'40'}/>
-            BOOK LOOM
-          </Typography>
+   <AppBar 
+  position="static" 
+  sx={{ background: "linear-gradient(90deg, rgb(0, 255, 200), rgb(2, 104, 117))" }}
+>
+  <Container maxWidth="xl">
+    <Toolbar disableGutters>
+      
+      {/* Desktop Logo */}
+      <Typography
+        variant="h6"
+        noWrap
+        component="a"
+        href="#"
+        sx={{
+          mr: 2,
+          display: { xs: 'none', md: 'flex' },
+          fontFamily: 'monospace',
+          fontWeight: 700,
+          letterSpacing: '.3rem',
+          color: 'white',
+          textDecoration: 'none',
+          alignItems: "center",
+          gap: 1
+        }}
+      >
+        <img 
+          src="https://i.pinimg.com/736x/ff/a9/32/ffa932be25e0bfa85d6c54b82bff0a27.jpg"  
+          alt="Book Loom Logo" 
+          width="40" 
+          height="40" 
+          style={{ borderRadius: "50%" }}  
+        />
+        BOOK LOOM
+      </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: 'block', md: 'none' } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+      {/* Mobile Menu */}
+      <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+        <IconButton
+          size="large"
+          aria-label="open navigation menu"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleOpenNavMenu}
+          color="inherit"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorElNav}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+          keepMounted
+          transformOrigin={{ vertical: 'top', horizontal: 'left' }}
+          open={Boolean(anchorElNav)}
+          onClose={handleCloseNavMenu}
+          sx={{ display: { xs: 'block', md: 'none' } }}
+        >
+          {pages.map((page) => (
+            <MenuItem key={page} onClick={handleCloseNavMenu}>
+              <Typography textAlign="center">{page}</Typography>
+            </MenuItem>
+          ))}
+        </Menu>
+      </Box>
+
+      {/* Mobile Logo */}
+      <Typography
+        variant="h5"
+        noWrap
+        component="a"
+        href="#"
+        sx={{
+          mr: 2,
+          display: { xs: 'flex', md: 'none' },
+          flexGrow: 1,
+          fontFamily: 'monospace',
+          fontWeight: 700,
+          letterSpacing: '.3rem',
+          color: 'white',
+          textDecoration: 'none',
+          alignItems: "center",
+          gap: 1
+        }}
+      >
+        <img 
+          src="https://i.pinimg.com/736x/ff/a9/32/ffa932be25e0bfa85d6c54b82bff0a27.jpg"  
+          alt="Book Loom Logo" 
+          width="30" 
+          height="30" 
+          style={{ borderRadius: "50%" }}  
+        />
+        BOOK LOOM
+      </Typography>
+
+      {/* Desktop Menu */}
+      <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+        {pages.map((page) => (
+          <Button
+            key={page}
+            onClick={handleCloseNavMenu}
+            sx={{ 
+              my: 2, 
+              color: 'white', 
+              display: 'block',
+              fontWeight: "500",
+              "&:hover": { backgroundColor: "rgba(255,255,255,0.1)" }
             }}
           >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            {page}
+          </Button>
+        ))}
+      </Box>
+
+      {/* User Menu */}
+      <Box sx={{ flexGrow: 0 }}>
+        <Tooltip title="Settings">
+          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+            <img 
+              src="https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png"  
+              alt="User Avatar"  
+              width={30} 
+              height={30} 
+              style={{ borderRadius: "50%" }}
+            />
+          </IconButton>
+        </Tooltip>
+        <Menu
+          sx={{ mt: '45px' }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+          keepMounted
+          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          {settings.map((setting) => (
+            <MenuItem key={setting} onClick={handleCloseUserMenu}>
+              <Typography textAlign="center">{setting}</Typography>
+            </MenuItem>
+          ))}
+        </Menu>
+      </Box>
+    </Toolbar>
+  </Container>
+</AppBar>
+
   );
  
 }
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,6 +5,8 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { FaArrowRight } from "react-icons/fa";
+
 
 function Landing() {
   return (
@@ -19,87 +21,236 @@ function Landing() {
         </div>
 
      </section> 
-     <section className='books my-5 '>
+     <section className='books  text-center ' style={{  marginBottom:"0%",background: "linear-gradient(90deg, rgb(225, 201, 247), rgb(2, 104, 117))"}}>
+  <div className='conatiner-fluid'>
+        <h2 className='text-light p-4'>NEW RELEASES</h2>
     
-     <div className='row container-fluid d-flex justify-content-evenly'>
-      <Card sx={{ maxWidth: 280 ,m:2,mx:"200px"}} className='col-3 mx-3'>
-        <CardMedia className='image-fluid'
-          sx={{ height: 280 , maxWidth: 240 ,mx:"auto"}}
-          image="https://5.imimg.com/data5/SELLER/Default/2024/9/451053603/BH/KA/SA/231929105/flipkart-images-7-png.png"
-          title="book"
-        />
-        <CardContent className=''>
-          <Typography gutterBottom variant="h5" component="div" className='d-flex justify-content-center'>
-            Atomic Habbit
-          </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }} className='d-flex justify-content-center'>
-            ₹450.00
-          </Typography>
-        </CardContent>
-        <CardActions className='d-flex justify-content-center'>
-          <Button size="small" className='center'>Add TO Cart</Button>
-         
-        </CardActions>
-      </Card>
-     <Card sx={{ maxWidth: 280 ,m: 2 ,mx:"200px"}} className='col-3 mx-3'>
-        <CardMedia className='image-fluid'
-          sx={{ height: 280 , maxWidth: 240 ,mx:"auto"}}
-          image="https://5.imimg.com/data5/SELLER/Default/2024/9/451053603/BH/KA/SA/231929105/flipkart-images-7-png.png"
-          title="book"
-        />
-        <CardContent className=''>
-          <Typography gutterBottom variant="h5" component="div" className='d-flex justify-content-center'>
-            Atomic Habbit
-          </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }} className='d-flex justify-content-center'>
-            ₹450.00
-          </Typography>
-        </CardContent>
-        <CardActions className='d-flex justify-content-center'>
-          <Button size="small" className='center'>Add TO Cart</Button>
-         
-        </CardActions>
-      </Card>
-     <Card sx={{ maxWidth: 280 ,m: 2,mx:"200px" }} className='col-3 mx-3'>
-        <CardMedia className='image-fluid'
-          sx={{ height: 280 , maxWidth: 240 ,mx:"auto"}}
-          image="https://5.imimg.com/data5/SELLER/Default/2024/9/451053603/BH/KA/SA/231929105/flipkart-images-7-png.png"
-          title="book"
-        />
-        <CardContent className=''>
-          <Typography gutterBottom variant="h5" component="div" className='d-flex justify-content-center'>
-            Atomic Habbit
-          </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }} className='d-flex justify-content-center'>
-            ₹450.00
-          </Typography>
-        </CardContent>
-        <CardActions className='d-flex justify-content-center'>
-          <Button size="small" className='center'>Add TO Cart</Button>
-         
-        </CardActions>
-      </Card>
-     <Card sx={{ maxWidth: 280 ,m: 2,mx:"200px" }} className='col-3 mx-3'>
-        <CardMedia className='image-fluid'
-          sx={{ height: 280 , maxWidth: 240 ,mx:"auto"}}
-          image="https://5.imimg.com/data5/SELLER/Default/2024/9/451053603/BH/KA/SA/231929105/flipkart-images-7-png.png"
-          title="book"
-        />
-        <CardContent className=''>
-          <Typography gutterBottom variant="h5" component="div" className='d-flex justify-content-center'>
-            Atomic Habbit
-          </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }} className='d-flex justify-content-center'>
-            ₹450.00
-          </Typography>
-        </CardContent>
-        <CardActions className='d-flex justify-content-center'>
-          <Button size="small" className='center'>Add TO Cart</Button>
-         
-        </CardActions>
-      </Card>
-     </div>
+       <div className='row container-fluid d-flex justify-content-evenly'>
+        <Card sx={{ maxWidth: 240, m: 2, mx: "auto" }} className="col-12 col-sm-6 col-md-4 col-lg-3">
+      <CardMedia
+        className="img-fluid"
+        sx={{ height: 280, maxWidth: 240, mx: "auto" }}
+        image="https://cdn.shopify.com/s/files/1/0666/3471/1191/files/9780008325947.jpg?v=1737522348"
+        title="book"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" className="d-flex justify-content-center">
+        Pride and Prejudice – paperback
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }} className="d-flex justify-content-center">
+          ₹450.00
+        </Typography>
+      </CardContent>
+      <CardActions className="d-flex justify-content-center">
+        <Button size="small">Add To Cart</Button>
+      </CardActions>
+        </Card>
+       <Card sx={{ maxWidth: 240, m: 2, mx: "auto" }} className="col-12 col-sm-6 col-md-4 col-lg-3">
+      <CardMedia
+        className="img-fluid"
+        sx={{ height: 280, maxWidth: 240, mx: "auto" }}
+        image="https://images.openai.com/thumbnails/4a3e92f08b26230c6b8376dbecc6250d.webp"
+        title="book"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" className="d-flex justify-content-center">
+         The Hobbit – paperback
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }} className="d-flex justify-content-center">
+          ₹750.00
+        </Typography>
+      </CardContent>
+      <CardActions className="d-flex justify-content-center">
+        <Button size="small">Add To Cart</Button>
+      </CardActions>
+    </Card>
+      <Card sx={{ maxWidth: 240, m: 2, mx: "auto" }} className="col-12 col-sm-6 col-md-4 col-lg-3">
+      <CardMedia
+        className="img-fluid"
+        sx={{ height: 280, maxWidth: 240, mx: "auto" }}
+        image="https://images.openai.com/thumbnails/fbbb6729e3859fe23e85f1a7fcc187fa.webp"
+        title="book"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" className="d-flex justify-content-center">
+         And Then There Were None
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }} className="d-flex justify-content-center">
+          ₹250.00
+        </Typography>
+      </CardContent>
+      <CardActions className="d-flex justify-content-center">
+        <Button size="small">Add To Cart</Button>
+      </CardActions>
+    </Card>
+      <Card sx={{ maxWidth: 240, m: 2, mx: "auto" }} className="col-12 col-sm-6 col-md-4 col-lg-3">
+      <CardMedia
+        className="img-fluid"
+        sx={{ height: 280, maxWidth: 240, mx: "auto" }}
+        image="https://dcbookstore.com/uploads/product/images/93434237102978-WhatsApp-Image-2025-06-19-at-5.25.41-PM.jpeg"
+        title="book"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" className="d-flex justify-content-center">
+          MULBERRY - ENNODU
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }} className="d-flex justify-content-center">
+          ₹500.00
+        </Typography>
+      </CardContent>
+      <CardActions className="d-flex justify-content-center">
+        <Button size="small">Add To Cart</Button>
+      </CardActions>
+    </Card>
+       </div>
+       <div className='row container-fluid d-flex justify-content-evenly'>
+        <Card sx={{ maxWidth: 240, m: 2, mx: "auto" }} className="col-12 col-sm-6 col-md-4 col-lg-3">
+      <CardMedia
+        className="img-fluid"
+        sx={{ height: 280, maxWidth: 240, mx: "auto" }}
+        image="https://dcbookstore.com/uploads/product/images/8275912835753-ujjayi.JPG"
+        title="book"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" className="d-flex justify-content-center">
+         UJJAYINIYILE
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }} className="d-flex justify-content-center">
+          ₹340.00
+        </Typography>
+      </CardContent>
+      <CardActions className="d-flex justify-content-center">
+        <Button size="small">Add To Cart</Button>
+      </CardActions>
+        </Card>
+       <Card sx={{ maxWidth: 240, m: 2, mx: "auto" }} className="col-12 col-sm-6 col-md-4 col-lg-3">
+      <CardMedia
+        className="img-fluid"
+        sx={{ height: 240, maxWidth: 240, mx: "auto" }}
+        image="https://dcbookstore.com/uploads/product/images/69053225884068-PREMANAGARAM.jpg"
+        title="book"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" className="d-flex justify-content-center">
+     PREMANAGARAM
+  
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }} className="d-flex justify-content-center">
+          ₹890.00
+        </Typography>
+      </CardContent>
+      <CardActions className="d-flex justify-content-center">
+        <Button size="small">Add To Cart</Button>
+      </CardActions>
+    </Card>
+      <Card sx={{ maxWidth: 240, m: 2, mx: "auto" }} className="col-12 col-sm-6 col-md-4 col-lg-3">
+      <CardMedia
+        className="img-fluid"
+        sx={{ height: 280, maxWidth: 240, mx: "auto" }}
+  image='https://dcbookstore.com/uploads/product/images/62831979338978-sundara.JPG'      title="book"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" className="d-flex justify-content-center">
+        SUNDARAJEEVITHAM
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }} className="d-flex justify-content-center">
+          ₹190.00
+        </Typography>
+      </CardContent>
+      <CardActions className="d-flex justify-content-center">
+        <Button size="small">Add To Cart</Button>
+      </CardActions>
+    </Card>
+      <Card sx={{ maxWidth: 240, m: 2, mx: "auto" }} className="col-12 col-sm-6 col-md-4 col-lg-3">
+      <CardMedia
+        className="img-fluid"
+        sx={{ height: 280, maxWidth: 240, mx: "auto" }}
+        image="https://5.imimg.com/data5/SELLER/Default/2024/9/451053603/BH/KA/SA/231929105/flipkart-images-7-png.png"
+        title="book"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" className="d-flex justify-content-center">
+          Atomic Habit
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }} className="d-flex justify-content-center">
+          ₹450.00
+        </Typography>
+      </CardContent>
+      <CardActions className="d-flex justify-content-center">
+        <Button size="small">Add To Cart</Button>
+      </CardActions>
+    </Card>
+       </div>
+       <div className='btn btn-outline-light text-center my-4' style={{borderRadius:"40px", width:"200px",height:"50px" }}><h2> View All <FaArrowRight />
+  </h2> </div>
+  </div>
      </section>
+    
+{/* img section */}
+<section className=' m-0 w-100' >
+  <img src="https://dcbookstore.com/uploads/homeslider/images/4864107377154-Rush-Hour-Banner.jpg" alt=""  className="img-fluid w-100" />
+  </section>
+
+
+   <section>
+  <h1 
+    className="text-center mb-4"
+  >
+    BROWSE BY AUTHORS
+  </h1>
+
+  <div className="row container mx-auto justify-content-center">
+    <div className="col-6 col-sm-4 col-md-3 col-lg-2 mb-3 d-flex justify-content-center" style={{width:"140px",height:"140px"}}>
+      <img src="https://dcbookstore.com/uploads/author/images/82014942448723-cv-balakrishnan..jpg" alt="author1" className="img-fluid " />
+    </div>
+    <div className="col-6 col-sm-4 col-md-3 col-lg-2 mb-3 d-flex justify-content-center" style={{width:"140px",height:"140px"}}>
+      <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRw4PEA0hrv-7V3j73zE9sjhv-7168wZpeWh-KKuoCAapG8p2BM" alt="author2" className="img-fluid " />
+    </div>
+    <div className="col-6 col-sm-4 col-md-3 col-lg-2 mb-3 d-flex justify-content-center" style={{width:"140px",height:"140px"}}>
+      <img src="https://keralainsider.com/wp-content/uploads/2020/07/pixlr_20200702184832776-799x1024.jpg" alt="author3" className="img-fluid " />
+    </div>
+    <div className="col-6 col-sm-4 col-md-3 col-lg-2 mb-3 d-flex justify-content-center" style={{width:"140px",height:"140px"}}>
+      <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRyLRQc7vJ_dqN23WTwd7SQ3T6M_w_C2HbYZ-BDzr5zVN_PiP_q" alt="author4" className="img-fluid "/>
+    </div>
+    <div className="col-6 col-sm-4 col-md-3 col-lg-2 mb-3 d-flex justify-content-center" style={{width:"140px",height:"140px"}}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/d/d4/Vijayan.jpg" alt="author5" className="img-fluid " />
+    </div>
+    <div className="col-6 col-sm-4 col-md-3 col-lg-2 mb-3 d-flex justify-content-center" style={{width:"140px",height:"140px"}}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Sugathakumari.jpg" alt="author6" className="img-fluid " />
+    </div>
+  </div>
+   {/* secod row */}
+    <div className="row container mx-auto justify-content-center">
+    <div className="col-6 col-sm-4 col-md-3 col-lg-2 mb-3 d-flex justify-content-center" style={{width:"140px",height:"140px"}}>
+      <img src="https://dcbookstore.com/uploads/author/images/43663833507128-arundhati-roy-rep1-1651742588.jpg" alt="author1" className="img-fluid " />
+    </div>
+    <div className="col-6 col-sm-4 col-md-3 col-lg-2 mb-3 d-flex justify-content-center" style={{width:"140px",height:"140px"}}>
+      <img src="https://dcbookstore.com/uploads/author/images/95288988167340-m-mukundan.jpg" alt="author2" className="img-fluid " />
+    </div>
+    <div className="col-6 col-sm-4 col-md-3 col-lg-2 mb-3 d-flex justify-content-center" style={{width:"140px",height:"140px"}}>
+      <img src="https://dcbookstore.com/uploads/author/images/42528547266911-KrMeera.jpg" alt="author3" className="img-fluid " />
+    </div>
+    <div className="col-6 col-sm-4 col-md-3 col-lg-2 mb-3 d-flex justify-content-center" style={{width:"140px",height:"140px"}}>
+      <img src="https://dcbookstore.com/uploads/author/images/99411929356387-padmarajan..jpg" alt="author4" className="img-fluid "/>
+    </div>
+    <div className="col-6 col-sm-4 col-md-3 col-lg-2 mb-3 d-flex justify-content-center" style={{width:"140px",height:"140px"}}>
+      <img src="https://dcbookstore.com/uploads/author/images/4503608526605-ns-madhavan.jpg" alt="author5" className="img-fluid " />
+    </div>
+    <div className="col-6 col-sm-4 col-md-3 col-lg-2 mb-3 d-flex justify-content-center" style={{width:"140px",height:"140px"}}>
+      <img src="https://dcbookstore.com/uploads/author/images/92133434021467-O.V.-Vijayan.jpg" alt="author6" className="img-fluid " />
+    </div>
+  </div>
+  <div className="text-center my-4">
+  <button 
+    className="btn btn-outline-dark px-4 py-2" 
+    style={{ borderRadius: "40px", minWidth: "200px", height: "50px" }}
+  >
+    <h5 className="mb-0">View More</h5>
+  </button>
+</div>
+</section>
+
+
     </>
   )
 }
